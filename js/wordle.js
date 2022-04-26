70f7df0f314fe7eb21767a06030d5ede3e61c58d
Roger Camargo <huogerac@gmail.com>
2022-04-26 19:54:47 -0300

    Adiciona validação multi linha

--- js/wordle.js
+++ js/wordle.js
@@ -1,5 +1,6 @@
 const PALAVRA_CORRETA = "PAUSA";
 
+let linha = 1;
 let palavraUsuario = [];
 
 var body = document.getElementsByTagName("body")[0];
@@ -7,7 +8,9 @@ var body = document.getElementsByTagName("body")[0];
 body.addEventListener("keyup", function (event) {
   let ehLetra = false;
   if (event.key == "Backspace" && palavraUsuario.length > 0) {
-    var el = document.getElementById("c" + (palavraUsuario.length - 1));
+    var el = document.getElementById(
+      "l" + linha + "c" + (palavraUsuario.length - 1)
+    );
     el.innerHTML = "";
     palavraUsuario.pop();
   } else if (event.key == "Enter" && palavraUsuario.length == 5) {
@@ -24,7 +27,7 @@ body.addEventListener("keyup", function (event) {
     for (let i in palavraUsuario) {
       console.log("===>", i);
       //if (palavraUsuario[i] ==  PALAVRA_CORRETA[i])
-      var el2 = document.getElementById("c" + i);
+      var el2 = document.getElementById("l" + linha + "c" + i);
       if (PALAVRA_CORRETA.indexOf(palavraUsuario[i]) == -1) {
         el2.classList.remove("br__cell--right");
         el2.classList.remove("br__cell--position");
@@ -41,13 +44,17 @@ body.addEventListener("keyup", function (event) {
         }
       }
     }
+    linha += 1;
+    palavraUsuario = [];
   } else if (palavraUsuario.length < 5) {
     palavraUsuario.push(event.key.toUpperCase());
     ehLetra = true;
   }
 
   if (ehLetra) {
-    var el = document.getElementById("c" + (palavraUsuario.length - 1));
+    var el = document.getElementById(
+      "l" + linha + "c" + (palavraUsuario.length - 1)
+    );
     el.innerHTML = event.key.toUpperCase();
   }
 });
